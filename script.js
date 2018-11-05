21e57f0c97875573ab81cbeaa746a7f4dc2d9c26
DclMaxLenire <43648539+DclMaxLenire@users.noreply.github.com>
2018-11-05 14:58:31 +0100

    quatrièmeVersion verification de l'adresse mail fonctionne

--- script.js
+++ script.js
@@ -32,6 +32,16 @@ if(dateDeNaissance >= 2001) {
     document.getElementById("dateDeNaissanceChamp").style.backgroundColor="green";
 }
 }    
+        // Verification de l'email //
+function verificationEmail() {
+        var testEmail =  /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+if (testEmail.test(document.getElementById("email").value)) {
+    document.getElementById("email").style.backgroundColor="green";
+} else {
+    document.getElementById("email").style.backgroundColor="red";
+}
+}
+        // Limité le nombre de caractère d'un message //
 
 
 
